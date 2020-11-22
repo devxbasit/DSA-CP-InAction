475e98895d22d2e339dfdb7340024c63f8b2d5c0
Mustafa-Basit <mustafa-basit@pop-os.localdomain>
2020-11-22 18:17:38 +0530

    Arrays in Action

---
+++ Arrays/ReverseArrayInGroups.java
@@ -0,0 +1,60 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+// Given an array arr[] of positive integers of size N. Reverse every sub-array group of size K.
+
+import java.io.*;
+import java.util.*;
+
+public class ReverseArrayInGroups {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+        
+        //while testcases exist
+        while(t-->0){
+            
+            String inputLine1[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine1[0]);
+            int k = Integer.parseInt(inputLine1[1]);
+            
+            ArrayList<Integer> arr = new ArrayList<>();            
+            String inputLine2[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                arr.add(Integer.parseInt(inputLine2[i]));
+            }
+            
+            Solution obj = new Solution();
+            obj.reverseInGroups(arr, n, k);
+            
+            StringBuffer str = new StringBuffer();
+            for(int i=0; i<n; i++){
+                str.append(arr.get(i) + " ");
+            }
+            System.out.println(str);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    void reverseInGroups(ArrayList<Integer> arr, int n, int k) {
+        // code here
+        for (int i = 0; i < n; i += k){
+            int left = i;
+            int right = Math.min(i + k - 1, n - 1);
+            
+            while (left < right){
+                int temp = arr.get(left);
+                arr.set(left, arr.get(right));
+                arr.set(right, temp);
+                left++;
+                right--;
+            }
+        }
+    }
+}
