42fd3d456dbabd4b9afdf23ae698c64e5430f085
Mustafa-Basit <mustafa-basit@pop-os.localdomain>
2020-12-27 16:07:29 +0530

    Initial commit

---
+++ Problems/5_LongestPalindromicSubstring.java
@@ -0,0 +1,30 @@
+class Solution {
+    int maxLen = 0;
+    int beginIndex;
+    
+    public void expandRange(String s, int begin, int end){
+        while (begin >= 0 && end < s.length() && s.charAt(begin) == s.charAt(end)){
+            begin--;
+            end++;
+        }
+        
+        if (maxLen < end - begin - 1){
+            maxLen = end - begin - 1;
+            beginIndex = begin + 1;
+        }
+    }   
+    
+    public String longestPalindrome(String s) {
+        
+        if (s.length() < 2)
+            return s;
+        
+        for (int start = 0; start < s.length() - 1; start++){
+            expandRange(s, start, start);
+            expandRange(s, start, start + 1);
+        }
+        
+        // start index is inclusive and end index is exclusive
+        return s.substring(beginIndex, beginIndex + maxLen);
+    }
+}
