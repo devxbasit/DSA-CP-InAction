80a910f5e6035c387f5c94e9ad72a8a7dc50c30f
Mustafa Basit <mailtobasit74@gmail.com>
2021-01-16 23:59:54 +0530

    hashing

---
+++ Hashing/CheckIfTwoArraysAreEqualOrNot.java
@@ -0,0 +1,86 @@
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		long t=sc.nextLong();
+		
+		while(t-->0)
+		{
+		  int n=sc.nextInt();
+		  long a[]=new long[n];
+		  long b[]=new long[n];
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      a[i]=sc.nextLong();
+		  }
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      b[i]=sc.nextLong();
+		  }
+		  
+		  System.out.println(check(a,b,n)==true?"1":"0");
+		  
+		  
+		}
+	}
+
+ // } Driver Code Ends
+
+
+//User function Template for Java
+
+
+public static boolean check(long arr[],long brr[],int n)
+{
+    //Your code here
+    
+     // simple approach - sort both arrays and comapare
+    
+    HashMap<Long, Integer> hm = new HashMap<>();
+    long key;
+    int freq;
+    
+    for (int i = 0; i < n; i++){
+        
+        freq = 1;
+        key = arr[i];
+        
+        if (hm.containsKey(key))
+            freq = hm.get(key) + 1;
+            
+        hm.put(key, freq);
+    }
+    
+    for (int i = 0; i < n; i++){
+        
+        key = brr[i];
+        
+        if (!hm.containsKey(key))
+            return false;
+            
+        freq = hm.get(key) - 1;
+        
+        if (freq == 0)
+            hm.remove(key);
+        else
+            hm.put(key, freq);
+    }
+    
+    return true;
+}
+
+// { Driver Code Starts.
+	    
+
+
+
+}  // } Driver Code Ends
---
+++ Hashing/MaxDistanceBetweenSameElements.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.util.*;
+
+class Max_Dis_Between_Same_Element
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t =sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+			GfG g = new GfG();
+			System.out.println(g.maxDistance(arr,n));
+		
+		t--;
+		}
+	}
+}// } Driver Code Ends
+
+
+// your task is to complete this function
+class GfG
+{
+    int maxDistance(int arr[], int n)
+    {
+    	// Your code here
+	
+	   Map<Integer, Integer> hm = new HashMap<>();
+	    
+	   int max = 0;
+	   int d;
+
+	   
+	   for (int i = 0; i < n; i++){
+	 
+	       if (hm.containsKey(arr[i]))
+	            max = Math.max(max, i - hm.get(arr[i]));
+	       else
+	            hm.put(arr[i], i);
+	   }
+	    
+	    return max;
+    }
+}
