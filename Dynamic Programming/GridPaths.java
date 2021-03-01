603354ae9835e2bda999fc465f6a96b494dea74b
Mustafa Basit <mailtobasit74@gmail.com>
2021-03-01 16:23:23 +0530

    solving cses problem set part 1 completed

---
+++ Dynamic Programming/GridPaths.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.util.*;
+
+class GridPaths {
+
+    public static int noOfWays(int board[][], int n) {
+
+        if (board[0][0] == -1 || board[n - 1][n -1] == -1) return 0;
+
+        board[n - 1][n - 1] = 1;
+
+        // fill last row 
+        for (int col = n - 2; col >= 0; col--) {
+
+            if (board[n - 1][col] == -1) {
+
+                board[n - 1][col] = 0;
+                continue;
+
+            }
+
+            board[n - 1][col] += board[n - 1][col + 1];
+
+        }
+
+        // fill last col 
+        for (int row = n - 2; row >= 0; row--) {
+
+            if (board[row][n - 1] == -1) {
+
+                board[row][n - 1] = 0;
+                continue;
+
+            }
+
+            board[row][n -1] += board[row + 1][n - 1];
+            
+        }
+
+
+
+        int MOD = 1000000007;
+
+        for (int row = n - 2 ; row >= 0; row--) {
+
+            for (int col = n - 2 ; col >= 0; col--) {
+
+                if (board[row][col] == -1) {
+                    
+                    board[row][col] = 0;
+                    continue;
+
+                }
+
+                board[row][col] = (board[row + 1][col] % MOD + board[row][col + 1] % MOD) % MOD;
+            }
+        }
+
+
+
+        return board[0][0];
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine().trim());
+
+        String S = "";
+
+        for (int i = 0; i < n; i++) S += br.readLine().trim();
+
+        int board[][] = new int[n][n];
+        int index = -1;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                if (S.charAt(++index) == '*') {
+
+                    board[i][j] = -1;
+
+                }
+            }
+        }
+        
+        System.out.println(noOfWays(board, n));
+
+    }
+}
---
+++ Dynamic Programming/RemovingDigits.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+class RemovingDigits {
+
+    public static int noOfSteps(int n) {
+
+        if (n == 0) return 0;
+        if (n < 10) return 1;
+
+        int dp[] = new int[n + 1];
+
+        dp[0] = 0;
+
+        for (int i = 1; i < dp.length; i++) {
+
+            dp[i] = Integer.MAX_VALUE;
+            
+        }
+        
+        for (int i = 1; i < dp.length; i++) {
+            
+            int num = i;
+
+            while(num > 0) {
+
+                int digit = num % 10;
+                num /= 10;
+
+                if (digit > i || digit == 0) continue;
+
+                if (dp[i - digit] == Integer.MAX_VALUE) continue;
+
+                dp[i] = Math.min(dp[i], 1 + dp[i - digit]);
+
+            }
+            
+        }
+        
+        return dp[dp.length - 1];
+        
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+        
+        System.out.println(noOfSteps(n));
+
+    }
+}
