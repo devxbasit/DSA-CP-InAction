eeb33461912fb8fb70ed6b580a5004d9b7024fb5
Mustafa Basit <mailtobasit74@gmail.com>
2021-03-15 21:46:51 +0530

    graphs

--- Problems/162. Find Peak Element.java
+++ Problems/162. Find Peak Element.java
@@ -1,38 +1,30 @@
 class Solution {
     
-    int binarySearch(int nums[], int low, int high) {
+    int binarySearch(int nums[], int n, int low, int high) {
         
-        if (low >= high) return low;
+        int mid = low + (high - low) / 2;
         
-        int mid = low + (high - low)  / 2;
+        if ((mid == 0 || nums[mid] > nums[mid - 1]) && (mid == n || nums[mid] > nums[mid + 1])) {
+            
+            return mid;
+            
+        }
         
-        if (mid != 0 && nums[mid] > nums[mid - 1]  &&
-                mid != nums.length - 1 && nums[mid] > nums[mid + 1]) return mid;
+        if (nums[mid] < nums[mid + 1]) {
+            
+            return binarySearch(nums, n, mid + 1, high);
+            
+            
+        }
         
-         
-        return nums[mid + 1] > nums[mid] ? binarySearch(nums, mid + 1, high) : 
-                                            binarySearch(nums, low, mid - 1);
-        
+        return binarySearch(nums, n, low, mid - 1);
         
     }
     
+    
     public int findPeakElement(int[] nums) {
         
-        if (nums.length == 1) return 0;
+        return binarySearch(nums, nums.length - 1, 0, nums.length - 1);
         
-        return binarySearch(nums, 0, nums.length - 1);
-
-        // simple iterative solution
-        // if (nums.length == 1) return 0;
-        
-        // int peak = 0;
-        
-        // for (int i = 0; i < nums.length - 1; i++)  {
-            
-        //     if(nums[i] > nums[i + 1]) return i;
-            
-        // }                
-
-        // return nums.length - 1;
     }
 }
---
+++ Problems/73. Set Matrix Zeroes.java
@@ -0,0 +1,82 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        
+        // method 1
+        // another method is use two 1D arrays, one for row and second for col
+        // and set corresponding row index or column index zero in 1D array 
+        
+        
+        // method 2
+        // use first row and first col of given array as indexes
+        
+        boolean firstColIsZero = false;
+        
+        for (int i = 0; i < matrix.length; i++) {
+            
+            for (int j = 0; j < matrix[0].length; j++) {
+                
+                if (matrix[i][j] == 0) {
+                    
+                    if (j == 0) {
+                        
+                        firstColIsZero = true;
+                        
+                        // mark row entry only
+                        matrix[i][0] = 0;
+                        continue;
+                        
+                    } 
+                    
+                    // row entry
+                    matrix[i][0] = 0;
+
+                    // col entry
+                    matrix[0][j] = 0;
+
+                }
+            }
+        }
+        
+        
+        // ***
+        // start from 1,1
+        
+        for (int i = 1; i < matrix.length; i++) {
+            
+            for (int j = 1; j < matrix[0].length; j++) {
+                
+                
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    
+                    matrix[i][j] = 0;
+                    
+                }       
+            }
+        }
+           
+        
+        
+        // check for first row
+        if (matrix[0][0] == 0) {
+            
+            for (int col = 0; col < matrix[0].length; col++) {
+                
+                matrix[0][col] = 0;
+                
+            }
+            
+        }
+        
+        // check for first col
+        if (firstColIsZero) {
+            
+            for (int row = 0; row < matrix.length; row++) {
+                
+                matrix[row][0] = 0;
+                
+            }
+             
+        }
+        
+    }
+}
