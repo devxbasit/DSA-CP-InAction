c50a271d43f2b52ae7f39efe6c2762bbb556330e
basit <devxbasit@gmail.com>
2022-12-21 22:02:38 +0530

    basic maths

---
+++ Problems/7. Reverse Integer - Approch1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int reverse(int x) {
+
+        int reversed = 0;
+        int digit = 0;
+
+        int maxLimit =  Integer.MAX_VALUE / 10;
+        int minLimit = Integer.MIN_VALUE / 10;
+
+        while (x != 0) {
+
+            digit = x % 10;
+            x = x / 10;
+
+            // int range in java -2,147,483,648 to 2,147,483,647 
+
+            // detect overflow 
+            if (reversed > maxLimit || (reversed == maxLimit && digit > 7)) return 0;
+            if (reversed < minLimit || (reversed == minLimit && digit > 8)) return 0;
+
+            reversed = reversed * 10 + digit;
+
+        }
+
+        return reversed;
+    }
+}
---
+++ Problems/7. Reverse Integer - Approch2.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=7 lang=java
+ *
+ * [7] Reverse Integer
+ */
+
+// @lc code=start
+
+class Solution {
+    public int reverse(int x) {
+
+        // https://www.youtube.com/watch?v=1bfbV4jx0To&ab_channel=CodingwithConner
+
+        int reversed = 0;
+        int digit = 0;
+
+        int maxLimit = Integer.MAX_VALUE / 10;
+        int minLimit = Integer.MIN_VALUE / 10;
+
+        while (x != 0) {
+
+            digit = x % 10;
+            x = x / 10;
+
+            int temp = reversed;
+
+            // see approach 1 - prevent overflow before hand
+
+            // cause overflow and then check whether overflow has happened or not
+            reversed = reversed * 10 + digit;
+
+            if ((reversed - digit) / 10 != temp)
+                return 0;
+        }
+
+        return reversed;
+    }
+}
+
+// @lc code=end
