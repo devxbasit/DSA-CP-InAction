7ceb9d5785f28ece326370953d7d249e4df7130a
basit <devxbasit@gmail.com>
2022-12-24 23:48:48 +0530

    Striver Guide Started

---
+++ Maths/Count Digits.java
@@ -0,0 +1,21 @@
+class Solution {
+    static int evenlyDivides(int N) {
+
+        // https://practice.geeksforgeeks.org/problems/count-digits5716/1
+
+        int num = N;
+        int count = 0;
+
+        while (num != 0) {
+
+            int digit = num % 10;
+
+            if (digit != 0 && N % digit == 0)
+                count++;
+
+            num = num / 10;
+        }
+
+        return count;
+    }
+}
