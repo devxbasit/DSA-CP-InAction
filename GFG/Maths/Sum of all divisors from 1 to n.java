dd46aa333eef477b854f2fd7705414d85531d2dc
basit <devxbasit@gmail.com>
2023-01-03 23:40:21 +0530

    multiples and divisors

---
+++ GFG/Maths/Sum of all divisors from 1 to n.java
@@ -0,0 +1,17 @@
+class Solution {
+    static long sumOfDivisors(int N) {
+        // code here
+        // https://www.youtube.com/watch?v=tDM6lT-qjys
+
+        int arr[] = new int[N + 1];
+
+        long sum = 0;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = i; j <= N; j += i) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
