b14957da771013fae3401b45065aec4218ed2c01
basit <devxbasit@gmail.com>
2023-01-21 17:29:42 +0530

    sorting basic

---
+++ GFG/Placement-Preparation-Course/Searching and Sorting/Bubble Sort.java
@@ -0,0 +1,23 @@
+// https://practice.geeksforgeeks.org/problems/bubble-sort/1
+
+class Solution {
+    // Function to sort the array using bubble sort algorithm.
+    public static void bubbleSort(int arr[], int n) {
+        // code here
+
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1])
+                    swap(arr, j, j + 1);
+            }
+        }
+    }
+
+    public static void swap(int arr[], int i, int j) {
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
