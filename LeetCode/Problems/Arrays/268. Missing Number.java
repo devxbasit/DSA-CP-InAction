80a05a642b90e29ec911d9d417323a401fd99461
Basit <devxbasit@gmail.com>
2023-06-07 07:52:34 +0530

    basic array questions

--- LeetCode/Problems/Arrays/268. Missing Number.java
+++ LeetCode/Problems/Arrays/268. Missing Number.java
@@ -1,19 +1,34 @@
 // https://leetcode.com/problems/missing-number/
 
-class Solution {
-
+ class Solution {
     public int missingNumber(int[] nums) {
 
-        int sum = 0;
+        Arrays.sort(nums);
+
+        return binarySearch(nums, 0, nums.length - 1);
+        
+    }
+
+    public int binarySearch(int[] nums, int low, int high) {
+
+        if (low > high) return low;
+
+        int mid = (high - low) / 2 + low;
+
+        if (mid != 0 && nums[mid - 1] == mid - 1 && nums[mid] != mid) return mid;
+
+        return (nums[mid] == mid) ? binarySearch(nums, mid + 1, high) : binarySearch(nums, low, mid - 1);
+    }
+}
+
+// Approach 2 - sum of first n numbers formula
+class Solution {
+    public int missingNumber(int[] nums) {
+        
         int n = nums.length;
+        int sum = 0;
+        for (int i = 0 ; i < nums.length; i++) sum += nums[i];
 
-        for (int i = 0; i < n; i++) {
-
-            sum += nums[i];
-
-        }
-
-        return (n * (n + 1)) / 2 - sum;
-
+        return (n * (n + 1) / 2) - sum;
     }
 }
--- LeetCode/Problems/Arrays/485. Max Consecutive Ones.java
+++ LeetCode/Problems/Arrays/485. Max Consecutive Ones.java
@@ -1,26 +1,46 @@
 // https://leetcode.com/problems/max-consecutive-ones/description/
 
+
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
+
+        int i = 0, j = 0;
+        int maxOnesCount = 0;
+
+        for (; j < nums.length; j++) {
+
+            if (nums[j] == 0) {
+                
+               maxOnesCount = Math.max(maxOnesCount, j - i);
+                i = j + 1;
+            } 
+        }
+
+        return Math.max(maxOnesCount, j - i);
+            
         
-        int max = 0;
-        int sum = 0;
+    }
+}
+ 
+ // approach 2
+ class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+
+        int maxOnesCount = 0, counter = 0;
 
         for (int i = 0; i < nums.length; i++) {
-
-            if (nums[i] == 0) {
-                
-                max = Math.max(sum, max);
-                sum = 0;
-
-            }  else  {
-
-                sum += nums[i];
-                
+            
+            if (nums[i] == 1) {
+                counter++; 
+                continue;
             }
 
+            if (counter > maxOnesCount) maxOnesCount = counter;
+
+            counter = 0;
         }
+
+        return counter > maxOnesCount ? counter : maxOnesCount;
         
-        return Math.max(sum, max);
     }
 }
